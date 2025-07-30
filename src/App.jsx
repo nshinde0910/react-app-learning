ec853a68b3c4aa982111b4f31ba99e6ede922c2d
Nishant Shinde <86105125+nshinde0910@users.noreply.github.com>
2025-07-30 13:33:55 +0530

    Testing 1.4

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1>Version 1.3</h1>
+      <h1>Version 1.4</h1>
       <h1>Vite + React + Nishant + Shinde</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
